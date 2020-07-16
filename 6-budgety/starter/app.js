6d256d0929256a1a75be174bd054771e077edc02
Colby Norman <colbynorm41@gmail.com>
2020-07-15 21:15:42 -0500

    added code to read the input data

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,22 +1,42 @@
 var budgetController = (function () {})();
 
 var UIController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputButton: ".add__btn",
+  };
+
   return {
     gitinput: function () {
-        
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
     },
   };
 })();
 
 var controller = (function (budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function () {
-    console.log("It Works!");
+    var input = UICtrl.getInput();
+    console.log(input);
+    //console.log("It Works!");
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document
+    .querySelector(DOM.inputButton)
+    .addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function (event) {
-    console.log(event);
+    //console.log(event);
 
     if (event.keyCode === 13) {
       console.log("ENTER was pressed");
