3baf907b36ea525896ddb35ad311c66b5b5b94b6
Colby Norman <colbynorm41@gmail.com>
2020-07-24 10:24:33 -0500

    updated budget in UI Controller

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -11,15 +11,25 @@ var budgetController = (function () {
     this.value = value;
   };
 
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(cur) {
+      sum += cur.value;
+    });
+    data.totals[type] = sum;
+  };
+
   var data = {
     allItems: {
       exp: [],
       inc: [],
     },
-    total: {
+    totals: {
       exp: 0,
-      inc: 0,
+      inc: 0
     },
+    budget: 0,
+    percentage: -1
   };
 
   return {
@@ -42,6 +52,30 @@ var budgetController = (function () {
       //return the new element
       return newItem;
     },
+
+    calculateBudget: function() {
+      //calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+      //calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+      //calculate the percentage of income that we spent
+      if (data.totals.inc > 0) {
+              data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      }else {
+        data.percentage = -1;
+      }
+    },
+
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      };
+    },
+
     testing: function () {
       console.log(data);
     },
@@ -120,8 +154,11 @@ var controller = (function (budgetCtrl, UICtrl) {
 
   var updateBudget = function () {
     //calculate budget
+    budgetCtrl.calculateBudget();
     //return the budget
+    var budget = budgetCtrl.getBudget();
     //display the budget on the UI
+    console.log(budget);
   };
 
   var ctrlAddItem = function () {
