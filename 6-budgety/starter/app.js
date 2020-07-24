e612cc110355ffd340db32929a311786ac0431fc
Colby Norman <colbynorm41@gmail.com>
2020-07-24 11:00:23 -0500

    updated budget in UI controller

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -11,9 +11,9 @@ var budgetController = (function () {
     this.value = value;
   };
 
-  var calculateTotal = function(type) {
+  var calculateTotal = function (type) {
     var sum = 0;
-    data.allItems[type].forEach(function(cur) {
+    data.allItems[type].forEach(function (cur) {
       sum += cur.value;
     });
     data.totals[type] = sum;
@@ -26,10 +26,10 @@ var budgetController = (function () {
     },
     totals: {
       exp: 0,
-      inc: 0
+      inc: 0,
     },
     budget: 0,
-    percentage: -1
+    percentage: -1,
   };
 
   return {
@@ -53,26 +53,26 @@ var budgetController = (function () {
       return newItem;
     },
 
-    calculateBudget: function() {
+    calculateBudget: function () {
       //calculate total income and expenses
-      calculateTotal('exp');
-      calculateTotal('inc');
+      calculateTotal("exp");
+      calculateTotal("inc");
       //calculate the budget: income - expenses
       data.budget = data.totals.inc - data.totals.exp;
       //calculate the percentage of income that we spent
       if (data.totals.inc > 0) {
-              data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
-      }else {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
         data.percentage = -1;
       }
     },
 
-    getBudget: function() {
+    getBudget: function () {
       return {
         budget: data.budget,
         totalInc: data.totals.inc,
         totalExp: data.totals.exp,
-        percentage: data.percentage
+        percentage: data.percentage,
       };
     },
 
@@ -87,9 +87,13 @@ var UIController = (function () {
     inputType: ".add__type",
     inputDescription: ".add__description",
     inputValue: ".add__value",
-    inputButton: ".add__btn",
-    inncomeContainer: ".income__list",
+    inputBtn: ".add__btn",
+    incomeContainer: ".income__list",
     expensesContainer: ".expenses__list",
+    budgetLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expensesLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
   };
 
   return {
@@ -106,16 +110,16 @@ var UIController = (function () {
       if (type === "inc") {
         element = DOMstrings.inncomeContainer;
         html =
-          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       } else if (type === "exp") {
         element = DOMstrings.expensesContainer;
         html =
-          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
       //REplace the placeholder text with some actual data
       newHTML = html.replace("%id%", obj.id);
       newHTML = newHTML.repalce("%description%", obj.description);
-      newHTML = newHTML.replace("%value%", obj.value);
+      newHTML = newHTML.replace("%value%", formatNumber(obj.value));
       //INsert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML("beforeend", newHTML);
     },
@@ -131,6 +135,18 @@ var UIController = (function () {
       fieldsArr[0].focus();
     },
 
+    displayBudget: function (obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentageLabel;
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = "---";
+      }
+    },
+
     getDOMstrings: function () {
       return DOMstrings;
     },
@@ -140,12 +156,9 @@ var UIController = (function () {
 var controller = (function (budgetCtrl, UICtrl) {
   var setupEventListeners = function () {
     var DOM = UICtrl.getDOMstrings();
-    document
-      .querySelector(DOM.inputButton)
-      .addEventListener("click", ctrlAddItem);
-    document.addEventListener("keypress", function (event) {
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function (event) {
       //console.log(event);
-
       if (event.keyCode === 13 || event.which === 13) {
         ctrlAddItem();
       }
@@ -158,6 +171,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     //return the budget
     var budget = budgetCtrl.getBudget();
     //display the budget on the UI
+    UICtrl.displayBudget(budget);
     console.log(budget);
   };
 
@@ -181,8 +195,14 @@ var controller = (function (budgetCtrl, UICtrl) {
   return {
     init: function () {
       console.log("application has started.");
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
-    },
+    }
   };
 })(budgetController, UIController);
 
