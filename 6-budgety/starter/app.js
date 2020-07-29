1c3004f048d997ddf75b2bf741804c488594b502
Colby Norman <colbynorm41@gmail.com>
2020-07-28 20:40:07 -0500

    added notes for percentages in UI

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -135,6 +135,7 @@ var UIController = (function () {
     expensesLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
     container: ".container",
+    expensesPercLabel: '.item__percentage'
   };
 
   return {
@@ -196,6 +197,24 @@ var UIController = (function () {
       }
     },
 
+    displayPercentages: function(percentages) {
+      var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+      
+      var nodeListForEach = function(list, callback) {
+        for ( var i = 0; i < list.length; i ++) {
+          callback(list[i], i);
+        }
+      };
+
+      nodeListForEach(fields, function(current, index) {
+        if (percentages[index] > 0) {
+                  current.textContent = percentages[index] + '%';
+        }else {
+          current.textContent = '---';
+        }
+      });
+    },
+
     getDOMstrings: function () {
       return DOMstrings;
     },
@@ -236,6 +255,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     //read percentages from the budget controller
     var percentages = budgetCtrl.getPercentages();
     //update the UI with the new percentages
+    UICtrl.displayPercentages(percentages);
     console.log(percentages);
   },
 
