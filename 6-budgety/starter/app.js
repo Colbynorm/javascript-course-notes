93d4ada41b55db206e2eee68019ee021bb80fc17
Colby Norman <colbynorm41@gmail.com>
2020-07-28 20:20:34 -0500

    added to budget controller

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -3,8 +3,21 @@ var budgetController = (function () {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
   };
 
+  Expense.prototype.calcPercentage = function(totalIncome) {
+    if (totalIncome > 0) {
+          this.percentage = Math.round((this.value / totalIncome) * 100);
+    } else {
+      this.percentage = -1;
+    }
+  };
+
+  Expense.prototype.getPercentage = function() {
+    return this.percentage;
+  };
+ 
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
@@ -79,6 +92,19 @@ var budgetController = (function () {
       } else {
         data.percentage = -1;
       }
+    },
+
+    calculatePercentages: function() {
+      data.allItems.exp.forEach(function(cur) {
+        cur.calcPercentage(data.totals.inc);
+      });
+    },
+
+    getPercentages: function() {
+      var allPerc = data.allItems.exp.map(function() {
+        return cur.getPercentages();
+      });
+      return allPerc;
     },
 
     getBudget: function () {
@@ -206,10 +232,11 @@ var controller = (function (budgetCtrl, UICtrl) {
 
   var updatePercentages = function() {
     //calculate percentages
-
+    budgetCtrl.calculatePercentages();
     //read percentages from the budget controller
-
+    var percentages = budgetCtrl.getPercentages();
     //update the UI with the new percentages
+    console.log(percentages);
   },
 
   var ctrlAddItem = function () {
