3cc27014d6ae81ee99848964c0bb5b8bd6465e78
Colby Norman <colbynorm41@gmail.com>
2020-08-03 17:58:10 -0500

    added arrow function notes

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -106,3 +106,34 @@ console.log(n.endsWith('n'));
 console.log(n.includes('lby'));
 console.log(firstName.repeat(5));
 console.log(`${firstName} `.repeat(5));
+
+
+
+
+
+
+
+
+// Arrow Functions
+
+const years = [1990, 1995, 2000, 2005];
+
+//ES5
+var ages5 = years.map(function(el) {
+    return 2020- el;
+});
+console.log(ages5);
+
+//ES6
+let ages6 = years.map(el => 2020 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2020 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`
+});
+console.log(ages6);
