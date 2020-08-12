9968fd417491d4d9cfd70c0a658c423e943a2c3a
Colby Norman <colbynorm41@gmail.com>
2020-08-11 21:32:08 -0500

    added rest paramaters

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -298,3 +298,20 @@ const boxes1 = document.querySelectorAll(".box");
 const all = [h, ...boxes1];
 
 //Array.from(all).forEach(cur => cur.syle.color = 'purple');
+
+//Rest Paramaters
+//ES5
+function isFullAge5(limit) {
+  var argsArr = Array.prototype.slice.call(arguments, 1);
+  argsArr.forEach(function (cur) {
+    console.log(2020 - cur >= limit);
+  });
+}
+isFullAge5(1990, 1999, 1965);
+
+//ES6
+function isFullAge6(limit, ...years) {
+  years.forEach((cur) => console.log(2020 - cur >= limit));
+  console.log(years);
+}
+isFullAge6(1990, 1999, 1965);
